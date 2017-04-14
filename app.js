0f39dac531467cc58cd5bd81f7dc33ab59f4ee0b
codemattcode <codemattcode@gmail.com>
2017-04-14 17:51:54 +0100

    cl

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 
 const app = express();
+
+const users = require('./routes/users');
+
 const port = 3000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/users', users);
 
 // Index route
 app.get('/', (req,res)=>{
