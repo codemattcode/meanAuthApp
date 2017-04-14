c04dc32f147843bc4088a7f9257d02b48ad26225
codemattcode <codemattcode@gmail.com>
2017-04-14 18:22:03 +0100

    add comments

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
+// Connect To Database
 mongoose.connect(config.database);
 
+// On Successul Connection to Database
 mongoose.connection.on('connected', ()=>{
   console.log('Connected to database ' + config.database);
+});
+
+// Error Connecting To Database
+mongoose.connection.on('error', (err)=>{
+  console.log('Database error: ' + err);
 });
 
 const app = express();
