4ed55464c1d828dc787bcf60513e67c3702f5ec3
codemattcode <codemattcode@gmail.com>
2017-04-14 17:47:56 +0100

    add comments

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 
 const app = express();
-
 const port = 3000;
 
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Index route
 app.get('/', (req,res)=>{
   res.send('Invalid endpoint /');
 });
 
+// Start server
 app.listen(port, ()=>{
   console.log('server started at ' + port)
 });
