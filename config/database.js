79563e4250d583c84526e2ddbb3b2365918d35eb
codemattcode <codemattcode@gmail.com>
2017-04-16 00:19:09 +0100

    linked mlab db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  // Uncomment for Development database
+  // database: 'mongodb://localhost:27017/meanauth',
+
+  // mLab.com for Heroku
+  database: 'mongodb://<fred>:<123456>@ds015713.mlab.com:15713/meanauthapp',
   secret: 'thesecret'
 }
