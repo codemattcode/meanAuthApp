a85dadb4d28de3c6db72a4d62ff2fec95bb260d3
codemattcode <codemattcode@gmail.com>
2017-04-16 00:03:30 +0100

    init

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -13,16 +13,16 @@ export class AuthService {
   registerUser(user){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    // observable
-    return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+    // observable : *removed http://localhost:3000/ for Heroku deployment*
+    return this.http.post('users/register', user, {headers: headers})
       .map(res => res.json());
   }
 
   authenticateUser(user){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    // observable
-    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+    // observable *removed http://localhost:3000/ for Heroku deployment*
+    return this.http.post('users/authenticate', user, {headers: headers})
       .map(res => res.json());
   }
 
@@ -31,8 +31,8 @@ export class AuthService {
     this.loadToken();
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type', 'application/json');
-    // observable
-    return this.http.get('http://localhost:3000/users/profile', {headers: headers})
+    // observable *removed http://localhost:3000/ for Heroku deployment*
+    return this.http.get('users/profile', {headers: headers})
       .map(res => res.json());
   }
 
