c7edbfe333279ecb6b49b4223ea9ec68af61ff73
codemattcode <codemattcode@gmail.com>
2017-04-15 02:44:40 +0100

    cl

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -12,9 +12,16 @@ export class AuthService {
   registerUser(user){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-
     // observable
     return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+      .map(res => res.json());
+  }
+
+  authenticateUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    // observable
+    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
       .map(res => res.json());
   }
 }
