c21b10db3440b40cfa569ad0424b5e320bb94822
codemattcode <codemattcode@gmail.com>
2017-04-15 02:39:20 +0100

    cl

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessageService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,10 @@ export class LoginComponent implements OnInit {
   username: String;
   password: String;
 
-  constructor(private authService:AuthService) { }
+  constructor(
+    private authService:AuthService,
+    private flashMessagingService:FlashMessageService
+  ) { }
 
   ngOnInit() {
   }
